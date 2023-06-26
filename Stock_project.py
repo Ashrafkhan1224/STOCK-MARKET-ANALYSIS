26fb90c285c291941b7e0e5e38dbc80b2e9580d0
MD ASHRAF KHAN <125546876+Ashrafkhan1224@users.noreply.github.com>
2023-06-26 19:13:19 +0530

    Add files via upload

---
+++ Stock_project.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Importing the Libraries
+
+# In[1]:
+
+
+import pandas as pd
+import yfinance as yf
+import datetime
+from datetime import date, timedelta
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+# In[2]:
+
+
+today = date.today()
+d1 = today.strftime("%Y-%m-%d")
+end_date= d1
+d2 = date.today() - timedelta(days=365)
+d2 = d2.strftime("%Y-%m-%d")
+start_date = d2
+data =  yf.download ('GOOG', start=start_date, end=end_date, progress=False)
+data["Date"] = data.index
+data = data[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
+data.reset_index(drop=True, inplace=True)
+print(data.head())
+
+
+# In[3]:
+
+
+figure = go.Figure(data=[go.Candlestick(x=data["Date"], open = data["Open"],
+                                        high=data["High"], low=data["Low"],
+                                        close=data["Close"])])
+
+figure.update_layout(title ="Google Stock Price Analysis", xaxis_rangeslider_visible=False)
+
+figure.show()
+
+
+# In[4]:
+
+
+figure = px.bar (data , x="Date",y="Close")
+figure.show()
+
+
+# In[5]:
+
+
+figure = px.line(data, x='Date', y='Close', title='Stock Market Analysis with Rangeslider')
+figure.update_xaxes (rangeslider_visible=True)
+figure.show()
+
+
+# In[6]:
+
+
+figure = px. line(data, x='Date', y='Close',title='Stock Market Analysis with Time Period Selectors')
+
+figure.update_xaxes( rangeselector=dict(buttons=list([dict(count=1, label="1m",
+                                step="month", stepmode="backward"), dict(count=6, 
+                                label="6m", step="month", stepmode="backward"), 
+                               dict(count=3, label="3m", step="month", stepmode="backward"), 
+                              dict(count=1, label="1y", step="year", stepmode="backward"), dict(step="all")])))
+
+figure.show()
+
+
+# In[7]:
+
+
+figure = px.scatter (data, x='Date', y='Close', range_x=['2022-06-20', '2023-06-19'],
+                     title= "Stock Market Analysis by Hiding Weekend Gaps")
+
+figure.update_xaxes( rangebreaks=[ dict(bounds=["sat", "sun"])])
+
+figure.show()
+
